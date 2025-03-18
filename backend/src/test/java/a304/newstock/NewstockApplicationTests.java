0f70fdd0c61c9e70be26aa1f134b450bc71da0da
nangdev <zovl5546@naver.com>
2025-03-18 12:17:27 +0900

    init

---
+++ backend/src/test/java/a304/newstock/NewstockApplicationTests.java
@@ -0,0 +1,13 @@
+package a304.newstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
