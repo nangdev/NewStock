8c18d83cb76c6a532c98acc5dd18850edee73945
nangdev <zovl5546@naver.com>
2025-03-26 20:44:56 +0900

    feat : 유저 관심 종목 수정 api 구현, 관심 종목 수정 에러코드 생성
    S12P21A304-91

---
+++ backend/src/main/java/newstock/domain/stock/dto/StockInfoDto.java
@@ -0,0 +1,48 @@
+package newstock.domain.stock.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import newstock.domain.stock.entity.Stock;
+
+import java.io.File;
+
+@Builder
+@Getter
+public class StockInfoDto {
+
+    private String stockName;
+
+    private Integer closingPrice;
+
+    private String rcPdcp;
+
+    private File stockImage;
+
+    private Integer totalPrice;
+
+    private String capital;
+
+    private String parValue;
+
+    private String issuePrice;
+
+    private String listingDate;
+
+    private String stdIccn;
+
+    public static StockInfoDto of(Stock stock) {
+        return StockInfoDto.builder()
+                .stockName(stock.getStockName())
+                .closingPrice(stock.getClosingPrice())
+                .rcPdcp(stock.getRcPdcp())
+                .stockImage(new File(stock.getImgUrl()))
+                .totalPrice(stock.getTotalPrice())
+                .capital(stock.getCapital())
+                .parValue(stock.getParValue())
+                .issuePrice(stock.getIssuePrice())
+                .listingDate(stock.getListingDate())
+                .stdIccn(stock.getStdIccn())
+                .build();
+    }
+
+}
--- backend/src/main/java/newstock/exception/ExceptionCode.java
+++ backend/src/main/java/newstock/exception/ExceptionCode.java
@@ -22,10 +22,11 @@ public enum ExceptionCode {
     NEWS_LETTER_NOT_FOUND("해당하는 뉴스레터가 존재하지 않습니다.", 3005),
     NEWS_SCRAP_NOT_FOUND("해당하는 뉴스 스크랩이 존재하지 않습니다.", 3006),
     NOTIFICATION_NOT_FOUND("해당하는 알림이 존재하지 않습니다.", 3007),
-    USER_STOCK_NOT_FOUND("해당하는 유저 주식이 존재하지 않습니다.", 3008),
+    USER_STOCK_NOT_FOUND("해당하는 유저 관심 종목이 존재하지 않습니다.", 3008),
     USER_NOTIFICATION_NOT_FOUND("해당하는 유저 알림이 존재하지 않습니다.", 3009),
     STOCK_ALREADY_EXISTS("이미 존재하는 주식입니다.", 3010),
-    USER_STOCK_ALREADY_EXISTS("이미 존재하는 유저 주식입니다.", 3011),
+    USER_STOCK_ALREADY_EXISTS("이미 존재하는 유저 관심 종목입니다.", 3011),
+    USER_STOCK_UPDATE_FAILED("유저 관심 종목 수정에 실패했습니다", 3012),
 
     // 외부 API 사용시 발생할 수 있는 예외 작성
     EXTERNAL_API_ERROR("외부 API를 호출하는 과정에서 예외가 발생했습니다.", 4001),
