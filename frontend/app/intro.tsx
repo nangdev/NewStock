39e48cb35b8007c517abd4cb419e540a2e667bec
aaaz425 <aaaz425@naver.com>
2025-03-26 17:25:20 +0900

    feat: 소개 페이지 작업
    - 소개 페이지 UI 작업
    
    S12P21A304-78

---
+++ frontend/app/intro.tsx
@@ -0,0 +1,39 @@
+import { AntDesign } from '@expo/vector-icons';
+import CustomButton from 'components/CustomButton';
+import { ROUTE } from 'constants/routes';
+import { useRouter } from 'expo-router';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+export default function Home() {
+  const router = useRouter();
+
+  const onPressLogo = () => {
+    router.navigate(ROUTE.HOME);
+  };
+
+  const onPressLogin = () => {
+    router.navigate(ROUTE.USER.LOGIN);
+  };
+
+  return (
+    <View className="flex-1 items-center justify-center gap-12">
+      <View className="items-center gap-4">
+        <TouchableOpacity onPress={onPressLogo}>
+          <Image
+            source={require('../assets/logo.png')}
+            style={{ width: 150, height: 150, resizeMode: 'contain' }}
+          />
+        </TouchableOpacity>
+        <View className="items-center">
+          <Text className="text-5xl font-bold text-primary">NewStock</Text>
+          <Text className="text-sm">주식 알림과 뉴스 레터를 동시에!</Text>
+        </View>
+      </View>
+
+      <CustomButton variant="semiRounded" onPress={onPressLogin} className="h-[45px] gap-4 px-10">
+        맞춤 주식 뉴스 보러 가기
+        <AntDesign name="arrowright" size={14} />
+      </CustomButton>
+    </View>
+  );
+}
