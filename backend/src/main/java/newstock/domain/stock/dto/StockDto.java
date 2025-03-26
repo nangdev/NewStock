8e19bc7568e602a685d3c4ec7415827a6452202c
nangdev <zovl5546@naver.com>
2025-03-26 10:36:13 +0900

    hotfix : 주식 서비스 구현미흡 수정, ExceptionResponse 클래스 삭제
    S12P21A304-91

--- backend/src/main/java/newstock/domain/stock/dto/StockDto.java
+++ backend/src/main/java/newstock/domain/stock/dto/StockDto.java
@@ -4,11 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 import newstock.domain.stock.entity.Stock;
 
+import java.io.File;
+
 @Builder
 @Getter
 public class StockDto {
     private Integer stockCode;
     private String stockName;
+    private File stockImage;
 
     public static StockDto fromStock(Stock stock) {
         return StockDto.builder()
