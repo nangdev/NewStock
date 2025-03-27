33f23317364de9da809c8f6312206441a7a97959
yeahhh1e <cakejuuu@gmail.com>
2025-03-27 14:48:12 +0900

    fix: isDuplicated 중복 출력 문제 수정
    - Lombok @Getter와 boolean 필드 조합으로 인한 JSON 필드 중복 직렬화 현상
      해결
    - @Getter -> @Data 사용
    - JsonProperty 사용
    
    S12P21A304-95

--- backend/src/main/java/newstock/controller/response/EmailCheckResponse.java
+++ backend/src/main/java/newstock/controller/response/EmailCheckResponse.java
@@ -2,11 +2,12 @@ package newstock.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
+@Data
 @AllArgsConstructor
 public class EmailCheckResponse {
 
+    @JsonProperty("isDuplicated")
     private boolean duplicated;
 }
