1aaf1b6b9b038aebf6a5dde308fa6c2340e8b23f
김용현 <dydgus0831@gmail.com>
2025-03-28 14:09:21 +0900

    refactor: 불필요 import문 제거

--- backend/src/main/java/newstock/domain/user/service/UserServiceImpl.java
+++ backend/src/main/java/newstock/domain/user/service/UserServiceImpl.java
@@ -20,8 +20,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import static newstock.domain.user.entity.QUser.user;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
