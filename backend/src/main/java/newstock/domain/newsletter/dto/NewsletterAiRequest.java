dbf6069692046237442a72cdc88b1cb7f19a3a2e
김용현 <dydgus0831@gmail.com>
2025-04-03 17:43:48 +0900

    feat: 뉴스레터 구현
    S12P21A304-240

--- backend/src/main/java/newstock/domain/newsletter/dto/NewsletterAiRequest.java
+++ backend/src/main/java/newstock/domain/newsletter/dto/NewsletterAiRequest.java
@@ -1,15 +1,4 @@
 package newstock.domain.newsletter.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class NewsletterAiRequest {
-
-    private
 }
