7054caf519cedbf6f426a46328fe2344919ba661
김가희 <gmlgml5023@naver.com>
2025-04-03 17:25:25 +0900

    deploy: 도커파일 workers 수정
    S12P21A304-237

--- ai/main.py
+++ ai/main.py
@@ -159,4 +159,4 @@ def get_keywords(request: KeywordRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
