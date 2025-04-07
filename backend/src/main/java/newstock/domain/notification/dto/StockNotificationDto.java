d8eb56292ba5cfe1cf5fb5186c25f9e9de3cb146
nangdev <zovl5546@naver.com>
2025-04-07 13:08:38 +0900

    fix : 전체 주식 조회 이미지 추가, 알림 도메인 dto 들 생성자 추가

--- backend/src/main/java/newstock/domain/notification/dto/StockNotificationDto.java
+++ backend/src/main/java/newstock/domain/notification/dto/StockNotificationDto.java
@@ -1,10 +1,14 @@
 package newstock.domain.notification.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StockNotificationDto {
 
     private Integer stockId;
--- backend/src/main/java/newstock/domain/stock/dto/StockDto.java
+++ backend/src/main/java/newstock/domain/stock/dto/StockDto.java
@@ -5,6 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import newstock.domain.stock.entity.Stock;
+import newstock.exception.ExceptionCode;
+import newstock.exception.type.InternalException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Base64;
 
 @Builder
 @NoArgsConstructor
@@ -18,6 +25,8 @@ public class StockDto {
 
     private String stockName;
 
+    private String stockImage;
+
     private boolean isInterested;
 
     public static StockDto of(Stock stock) {
@@ -25,7 +34,22 @@ public class StockDto {
                 .stockId(stock.getStockId())
                 .stockCode(stock.getStockCode())
                 .stockName(stock.getStockName())
+                .stockImage(getBase64Image(stock.getImgUrl()))
                 .build();
+    }
+
+    private static String getBase64Image(String imgUrl) {
+        File imageFile = new File(imgUrl);
+
+        byte[] fileContent;
+
+        try {
+            fileContent = Files.readAllBytes(imageFile.toPath());
+        } catch (IOException e) {
+            throw new InternalException(ExceptionCode.STOCK_IMAGE_CHANGE_FAIELD);
+        }
+
+        return Base64.getEncoder().encodeToString(fileContent);
     }
 
 }
