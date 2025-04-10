6ad39d52f6722995e8f4c0a56c3b196ab2ecd2fa
yeahhh1e <cakejuuu@gmail.com>
2025-04-10 20:28:17 +0900

    design: 뉴스 스크랩 상세 페이지
    S12P21A304-327

--- frontend/components/news/SortButton.tsx
+++ frontend/components/news/SortButton.tsx
@@ -20,13 +20,13 @@ export default function SortButton({ sort, setSort }: Props) {
     <View className="relative">
       <Pressable
         onPress={() => setIsOpen(!isOpen)}
-        className="flex-row items-center justify-center rounded-full bg-blue-100 px-3 py-1 pl-4">
+        className="h-[30px] flex-row items-center justify-center rounded-full bg-blue-100 px-3 py-1 pl-4">
         <Text className="mr-1 text-sm font-medium text-blue-800">{selectedLabel}</Text>
         <AntDesign name="down" size={12} color="#1d4ed8" />
       </Pressable>
 
       {isOpen && (
-        <View className="bg-whiteke absolute top-full z-50 w-full rounded-lg border border-gray-100  bg-white shadow-md">
+        <View className="absolute top-[30px] z-50 mr-1 w-full rounded-lg border border-gray-100 bg-white shadow-md">
           {options.map((option) => {
             const isSelected = option.value === sort;
             return (
